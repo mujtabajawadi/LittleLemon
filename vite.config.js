5e30a9dbd83445227b03c9e7a242683dcf565a85
Muhammad Mujtaba Jawadi <mujtabafalcon986@gmail.com>
2025-11-22 15:39:13 +0500

    fix: Update base path in Vite config and remove server configuration

--- vite.config.js
+++ vite.config.js
@@ -7,15 +7,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/LittleLemon",
+  base: "/",
   test: {
     globals: true,
     environment: "jsdom",
     setupFiles: "src/__tests__/setup.js",
-  },
-  server: {
-    host: "0.0.0.0", // This allows external connections
-    port: 5173,
-    strictPort: true, // Prevents automatic port switching
   },
 });
